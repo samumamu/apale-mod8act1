443fce2a1151c099ff5ab071fbdaea9329e1866d
Samuel Ray Apale <92353628+samumamu@users.noreply.github.com>
2023-02-28 14:37:32 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,43 @@
+var fname = document.getElementById('fname');
+var gender = document.getElementById('gender');
+var username = document.getElementById('username');
+var desc = document.getElementById('desc');
+var year = document.getElementById('year');
+var age = document.getElementById('age');
+var ppic = document.getElementById('ppic');
+var canorcon;
+
+fname.innerHTML = prompt('Please Type your Full Name:');
+sex();
+username.innerHTML = prompt('Please Type your Username:');
+desc.innerHTML = prompt('Write a brief description about yourself:');
+year.innerHTML = prompt('What year were you born?');
+age.innerHTML = prompt('How old are you?');
+pfp();
+
+function sex()
+{
+    var set;
+    var gen;
+    if(canorcon = confirm('What is Your Gender?\nClick Ok if M\nClick Cancel if F'))
+    {
+        set = alert('Your Gender is set to M!');
+        gen = 'M';
+    }
+    else
+    {
+        set = alert('Your Gender is set to F!');
+        gen = 'F';
+    }
+    document.getElementById('gender').innerHTML = gen;
+}
+
+function pfp()
+{
+    var pfpsrc;
+    if(canorcon = confirm('Do you want to set a profile picture?'))
+    {
+        pfpsrc = prompt('Please type the link/source of the image:');
+    }
+    ppic.src = pfpsrc;
+}
